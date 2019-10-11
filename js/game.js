d08f05d558aaf04b12f33ee809c3dc2c46170808
crownless001 <55293353+crownless001@users.noreply.github.com>
2019-10-11 23:08:42 +0400

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,52 @@
+const numDivs = 36;
+const maxHits = 10;
+
+let hits = 0;
+let firstHitTime = 0;
+
+function round() {
+  // FIXME: надо бы убрать "target" прежде чем искать новый
+  $(".target").removeClass('target');
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  // TODO: помечать target текущим номером
+   $(divSelector).text(hits);
+  // FIXME: тут надо определять при первом клике firstHitTime
+  if (hits === 1) { firstHitTime = getTimestamp(); }
+  if (hits === maxHits) {
+    endGame();
+  }
+}
+
+function endGame() {
+  // FIXME: спрятать игровое поле сначала
+  $(".game-field").addClass('d-none');
+
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+
+  $("#win-message").removeClass("d-none");
+}
+
+function handleClick(event) {
+  // FIXME: убирать текст со старых таргетов. Кажется есть .text?
+  if ($(event.target).hasClass("target")) {
+    hits = hits + 1;
+    $(event.target).text("");
+    round();
+  }
+  // TODO: как-то отмечать если мы промахнулись? См CSS класс .miss
+}
+
+function init() {
+  // TODO: заказчик просил отдельную кнопку, запускающую игру а не просто по загрузке
+  round();
+
+  $(".game-field").click(handleClick);
+  $("#button-reload").click(function() {
+    location.reload();
+  });
+}
+
+$(document).ready(init);
